7e1536ec5d62e1c2769b12ef082139e9156c86a3
sp41mer <sp41mer@yahoo.com>
2016-08-21 18:06:59 +0300

    for heroku

--- DigitalAgency/settings.py
+++ DigitalAgency/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,6 +83,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#Database (Heroku)
+#uncomment that for heroku deploy, pel'meshek
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
